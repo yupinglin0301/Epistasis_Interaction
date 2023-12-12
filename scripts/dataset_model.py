08d9fa27f0ea936a93c0f3a1e0cec812175c122d
yupinglin0301 <yupinlin50@gmail.com>
2023-12-12 20:27:15 +0800

    feats:Add group_shuffle_split function and create tran_test_split mixin, group_shuffle_split mixin

--- scripts/dataset_model.py
+++ scripts/dataset_model.py
@@ -3,8 +3,6 @@ from  pathlib import Path
 import joblib
 import numpy as np
 import pandas as pd
-import yaml
-import sys
 
 
 class ExpressionDataset(ABC):
@@ -61,125 +59,109 @@ class ExpressionDataset(ABC):
          Load the version of the pipline
          """
          raise NotImplementedError
+     
+class TrainTestSplitMixin():
+    """
+    A mixin class providing train-test splitting functionality
+    """
     
+    def shuffle_data(self, X, y, seed):
+       """
+       Random shuffle of the samples in X and y
+       """
+       np.random.seed(seed)
+       idx = np.arange(X.shape[0])
+       np.random.shuffle(idx)
+       
+       return X[idx], y[idx]   
+
+    def train_test_split(self, X, y, seed, test_size=0.2):
+        """
+        Split the data into train and test sets
+        """
+        X, y = self.shuffle_data(X, y, seed)
+        split_i = len(y) - int(len(y) // (1 / test_size))
+        X_train, X_test = X[:split_i], X[split_i:]
+        y_train, y_test = y[:split_i], y[split_i:]
+        
+        return X_train, X_test, y_train, y_test
+
+class GroupShuffleSplitMixin():
+    """
+    A mixin class providing group shuffle splitting functionality
+    """
+    
+    def group_shuffle_split(self, X, y, groups, seed, n_splits=1 , test_size=0.2):
+        """
+        Split the data into train and test sets
+        """
+        
+        from sklearn.model_selection import GroupShuffleSplit
+        gss = GroupShuffleSplit(n_splits=n_splits, train_size=test_size, random_state=seed)  
+        split = gss.split(X, y, groups=groups)
+        train_ids, test_ids = next(split)
+        
+        X_train, X_test = X[train_ids], X[test_ids]
+        y_train, y_test = y[train_ids], y[test_ids]
+        
+        return X_train, X_test, y_train, y_test
     
 
-class TrainTestSplit(ExpressionDataset):
-    """
-     A base train_test_split defining the API for train-test splitting
-    """
-    @abstractmethod
-    def train_test_split(self,
-                         train_fraction,
-                         test_fraction,
-                         seed):
-        """
-        Split the dataset into two portion, 
-        as seen in scikit-learn's `train_test_split` function
-        """
-         
-        raise NotImplementedError
-    
-    @abstractmethod
-    def shuffle_data(self, X, y, seed):
-        """
-        Random shuffle of the samples in X and y
-        """
-        raise NotImplementedError
-    
-    
-class RF_OOB_Dataset(TrainTestSplit):
+class RF_OOB_Dataset(ExpressionDataset, TrainTestSplitMixin, GroupShuffleSplitMixin):
     """
     A class containing logic used by all the types of gwas datasets for computing out of bag score
-    The RF_OOB_Dataset inheritance pattern from class ExpressionDataset and TrainTestSplit
+    The RF_OOB_Dataset inheritance pattern from class ExpressionDataset, TrainTestSplitMixin and GropShufflesSplitMixin
     """
-    
-    def __init__(self,
-                 gwas_gen_dir,
-                 label_df_dir,
-                 env_df_dir):
-        
+
+    def __init__(self, gwas_gen_dir, label_df_dir, env_df_dir):
         """
         An initializer for the class
         """
-        
         self.all_gen_df = pd.read_csv(gwas_gen_dir, sep=",")
-        self.all_gen_df = self.all_gen_df.drop(['FID','IID'], axis=1)
+        self.all_gen_df = self.all_gen_df.drop(['FID', 'IID'], axis=1)
         self.env_df = pd.read_csv(env_df_dir, sep="\t")
-        
         self.all_gwas_df = pd.concat([self.all_gen_df, self.env_df], axis=1)
         self.label_df = pd.read_csv(label_df_dir, sep="\t")
-    
-     
-    @classmethod 
-    def from_config(class_object,
-                    config_file,
-                    weight_tissue):
+
+    @classmethod
+    def from_config(cls, config_file, weight_tissue):
         """
         A function to create a new object from paths to its data
         """
-        
-            
         data_dir = Path(config_file['dataset']['data_dir'])
         gwas_df_dir = data_dir / weight_tissue / ("predict_expression_" + weight_tissue + "_output.csv")
-        
-        return class_object(gwas_df_dir, config_file['dataset']['phentoype_dir'], config_file['dataset']['env_dir'])	    
-    
+        return cls(gwas_df_dir, config_file['dataset']['phentoype_dir'], config_file['dataset']['env_dir'])
+
     def get_samples(self):
         """
         Return the list of sample accessions for all samples currently available in the dataset
         """
         return list(self.all_gwas_df.index)
-    
+
     def get_features(self):
         """
         Return the list of the ids of all the features in the currently available in the dataset 
         """
         return list(self.all_gwas_df.columns)
-    
-    def shuffle_data(self, X, y, seed):
-        """
-        Random shuffle of the samples in X and y
-        """
-        
-        np.random.seed(seed)
-        idx = np.arange(X.shape[0])
-        np.random.shuffle(idx)
-        
-        return X[idx], y[idx]
-    
-    def train_test_split(self, X, y, seed, test_size=0.2):
-        """
-        Split the data into train and test sets
-        """
-        
-        X, y = self.shuffle_data(X, y, seed)
-        split_i = len(y) - int(len(y)// (1 / test_size))
-        X_train, X_test = X[:split_i], X[split_i:]
-        y_train , y_test = y[:split_i], y[split_i:]
-        
-        return X_train, X_test, y_train, y_test
-    
+
     def generate_labels(self, phen_trait):
         """
-        Random shuffle of the samples in X and y
+        Process the y matrix for the given phenotype trait
         """
         y_given_phen = self.label_df.loc[:, [phen_trait]]
-        
         return y_given_phen
-    
+
     @staticmethod
     def save_pipeline(pipeline_to_save, save_file_name):
-         """
-         Save the version of the pipline
-         """
-         joblib.dump(pipeline_to_save, save_file_name)
-         
-    @staticmethod
-    def load_pipeline(pipline_file_path):
-         """
-         Load the version of the pipline
-         """		
-         pipline_file_path  = joblib.load(filename=pipline_file_path)
-         return pipline_file_path
+        """
+        Save the version of the pipeline
+        """
+        joblib.dump(pipeline_to_save, save_file_name)
 
+    @staticmethod
+    def load_pipeline(pipeline_file_path):
+        """
+        Load the version of the pipeline
+        """
+        pipeline = joblib.load(filename=pipeline_file_path)
+        return pipeline
