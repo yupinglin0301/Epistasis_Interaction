15a4e56b39250de141bafca282e325ece0b87e0e
yupinglin0301 <yupinlin50@gmail.com>
2023-12-29 13:55:37 +0800

    feats: Add GTEX_raw_Dataset class

--- scripts/dataset_model.py
+++ scripts/dataset_model.py
@@ -47,14 +47,14 @@ class ExpressionDataset(ABC):
         raise NotImplementedError
     
     @abstractmethod
-    def save_pipeline(self):
+    def save(self):
          """
          Save the version of the pipline
          """
          raise NotImplementedError
      
     @abstractmethod
-    def load_pipeline(self):
+    def load(self):
          """
          Load the version of the pipline
          """
@@ -152,16 +152,75 @@ class RF_OOB_Dataset(ExpressionDataset, TrainTestSplitMixin, GroupShuffleSplitMi
         return y_given_phen
 
     @staticmethod
-    def save_pipeline(pipeline_to_save, save_file_name):
+    def save(pipeline_to_save, save_file_name):
         """
         Save the version of the pipeline
         """
         joblib.dump(pipeline_to_save, save_file_name)
 
     @staticmethod
-    def load_pipeline(pipeline_file_path):
+    def load(pipeline_file_path):
         """
         Load the version of the pipeline
         """
         pipeline = joblib.load(filename=pipeline_file_path)
         return pipeline
+    
+class GTEX_raw_Dataset(ExpressionDataset):
+    """
+    A class containing logic used by all the types of gwas datasets for computing out of bag score
+    The RF_OOB_Dataset inheritance pattern from class ExpressionDataset, TrainTestSplitMixin and GropShufflesSplitMixin
+    """
+
+    def __init__(self, gwas_gen_dir, label_df_dir, env_df_dir):
+        """
+        An initializer for the class
+        """
+        self.all_gen_df = pd.read_csv(gwas_gen_dir, sep=",")
+        self.all_gen_df = self.all_gen_df.drop(['FID', 'IID'], axis=1)
+        self.env_df = pd.read_csv(env_df_dir, sep="\t")
+        self.all_gwas_df = pd.concat([self.all_gen_df, self.env_df], axis=1)
+        self.label_df = pd.read_csv(label_df_dir, sep="\t")
+
+    @classmethod
+    def from_config(cls, config_file, weight_tissue):
+        """
+        A function to create a new object from paths to its data
+        """
+        data_dir = Path(config_file['dataset']['data_dir'])
+        gwas_df_dir = data_dir / weight_tissue / (weight_tissue + "_imputed.txt")
+        return cls(gwas_df_dir, config_file['dataset']['phentoype_dir'], config_file['dataset']['env_dir'])
+
+    def get_samples(self):
+        """
+        Return the list of sample accessions for all samples currently available in the dataset
+        """
+        return list(self.all_gwas_df.index)
+
+    def get_features(self):
+        """
+        Return the list of the ids of all the features in the currently available in the dataset 
+        """
+        return list(self.all_gwas_df.columns)
+
+    def generate_labels(self, phen_trait):
+        """
+        Process the y matrix for the given phenotype trait
+        """
+        y_given_phen = self.label_df.loc[:, [phen_trait]]
+        return y_given_phen
+
+    @staticmethod
+    def save(save_df, save_file_name):
+        """
+        Save the preprocessed file
+        """
+        save_df.to_csv(save_file_name, sep='\t')
+
+    @staticmethod
+    def load(save_file_path):
+        """
+        Load the preprocessed file
+        """
+        preprocessed_df = pd.read_csv(filename=save_file_path, sep='\t')
+        return preprocessed_df
