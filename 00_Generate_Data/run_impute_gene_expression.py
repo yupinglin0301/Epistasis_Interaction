153609b8b7ca879d9f92cac40da0c702f6e25d19
yupinglin0301 <yupinlin50@gmail.com>
2023-12-29 14:04:14 +0800

    feats:revise preprocessing part

--- 00_Generate_Data/run_impute_gene_expression.py
+++ 00_Generate_Data/run_impute_gene_expression.py
@@ -6,7 +6,7 @@ Usage:
     nohup python run_impute_gene_expression.py \
       --data_config "/exeh_4/yuping/Epistasis_Interaction/00_Generate_Data/config.yaml" \
       --data_dir "/mnt/data/share/yuping" \
-      --work_dir "/exeh_4/yuping/Epistasis_Interaction" \
+      --work_dir "/exeh_4/yuping/Epistasis_Interaction/00_Generate_Data" \
       --weight_tissue "Brain_Amygdala" \
       --weight_end_prefix ".db" \
       --weight_prefix "gtex_v7" \
@@ -21,6 +21,8 @@ gene expression for specifice tissue
 import argparse
 import utils
 import impute_predixcan as im
+import sys
+import yaml
 from pathlib import Path
 
   
@@ -90,31 +92,58 @@ if __name__ == '__main__':
     # Set up logging
     logger = utils.logging_config(input_arguments.weight_tissue)
     
-    logger.info("Checking arguments and creating experiment dir/file ... ")
-    # Loading configure file and checking all arguments
-    data_configure = utils.load_config_and_check_arg(input_arguments)
-    # Create experiment dir 
-    experiment_dir = Path(data_configure['save_dir'], data_configure['weight_tissue']).resolve()
-    experiment_dir.mkdir(parents=True, exist_ok=True)
-    output_filename = utils.construct_filename(experiment_dir, "imputed", ".txt", data_configure['weight_tissue'])
-    # Check if the output filename already exists
-    if utils.check_exist_files(output_filename):
-        raise Exception("Results file " + str(output_filename) + " exist already. Please double-check.")
+    logger.info("Check if all files and directories exist ... ")
+    
+    # Loading configuration file
+    try:
+        with open(input_arguments.data_configure) as infile:
+            load_configure = yaml.safe_load(infile)
+    except Exception:
+            sys.stderr.write("Please specify valid yaml file.")
+            sys.exit(1)
 
+    data_dir = Path(input_arguments.data_dir)
+    work_dir = Path(input_arguments.work_dir)
+
+    save_dir = work_dir.joinpath("results")
+    genotype_dir = data_dir.joinpath(load_configure['genotype_data_path'])
+    weight_tissue_dir = data_dir.joinpath(load_configure['weights_data_path'])
+    
+    dosage_prefix = input_arguments.dosage_prefix
+    weight_prefix = input_arguments.weight_prefix
+    dosage_end_prefix = input_arguments.dosage_end_prefix
+    weight_end_prefix = input_arguments.weight_end_prefix
+    weight_tissue = input_arguments.weight_tissue
+    
+    sample_data = data_dir.joinpath(load_configure['sample_data'])
+    reference_data = data_dir.joinpath(load_configure['reference_data'])
+    beta_file = weight_tissue_dir / (weight_prefix + "_" +  weight_tissue + "_imputed_europeans_tw_0.5_signif" +  weight_end_prefix)
+    
+    
+    if not utils.check_exist_files([sample_data, reference_data, beta_file]):
+        raise Exception("See output above. Problems with specified directories")
+    if not utils.check_exist_directories(save_dir):
+        raise Exception("The directory" + str(save_dir) + "not exist. Please double-check.")
+    else:
+        # Create experiment directory
+        experiment_dir = Path(save_dir, input_arguments.weight_tissue).resolve()
+        experiment_dir.mkdir(parents=True,  exist_ok=True)
+        
+        output_filename = utils.construct_filename(experiment_dir, "imputed", ".txt", input_arguments.weight_tissue)
+        # Check output file is exist or not
+        if utils.check_exist_files(output_filename):
+            raise Exception("Results file " + str(output_filename) + " exist already. Please double-check.")
+        
     
     logger.info("Imputing Gene Expression ... ")
 
-    beta_file = data_configure['weight_tissue_dir'] / (data_configure['weight_prefix'] + "_" + data_configure['weight_tissue'] +"_imputed_europeans_tw_0.5_signif" + data_configure['weight_end_prefix'])
-    sample_file =  data_configure['sample_data']
-    reference_file = data_configure['reference_data']
-
     unqiue_rsids = im.GenotypeDataset.UniqueRsid(beta_file)
-    TranscriptionMatrix = im.TranscriptionMatrix(beta_file, sample_file)
-    reference_file = im.GenotypeDataset.get_reference(reference_file)
+    TranscriptionMatrix = im.TranscriptionMatrix(beta_file, sample_data)
+    reference_file = im.GenotypeDataset.get_reference(reference_data)
 
-    for rsid, allele, dosage_row in im.GenotypeDataset.get_all_dosages(data_configure['genotype_dir'],
-                                                                       data_configure['dosage_prefix'],
-                                                                       data_configure['dosage_end_prefix'],
+    for rsid, allele, dosage_row in im.GenotypeDataset.get_all_dosages(genotype_dir, 
+                                                                       dosage_prefix, 
+                                                                       dosage_end_prefix,
                                                                        unqiue_rsids,
                                                                        reference_file,
                                                                        logger):
