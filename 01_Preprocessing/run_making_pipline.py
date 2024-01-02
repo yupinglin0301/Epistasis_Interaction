0eeea0d31d56a7762bf118f7d930641be8d2551f
yupinglin0301 <yupinlin50@gmail.com>
2024-01-02 23:14:59 +0800

    feats:add normalized function

--- 01_Preprocessing/run_making_pipline.py
+++ 01_Preprocessing/run_making_pipline.py
@@ -4,6 +4,7 @@ import dataset_model as dm
 import preprocessors as pp
 import argparse
 import utils
+import joblib
 
 
 """
@@ -13,7 +14,8 @@ Usage:
     
     nohup python run_making_pipline.py \
       --work_dir "/exeh_4/yuping/Epistasis_Interaction/01_Preprocessing" \
-      --weight_tissue "Brain_Amygdala" >  /exeh_4/yuping/Epistasis_Interaction/01_Preprocessing/Log/nohup.txt &
+      --weight_tissue "Brain_Amygdala" \
+      --normalized  >  /exeh_4/yuping/Epistasis_Interaction/01_Preprocessing/Log/nohup.txt &
       
 Output:
 Feature engineering pipeline for specific imputed brain tissue and environmental factors
@@ -37,6 +39,12 @@ def process_args():
         help="Data directory where the phenotype and genotype matrix are stored."
     )
     
+    parser.add_argument(
+        '--normalized',
+        action="store_true",
+        help="Normalized the gene expression dataset or not."
+    )
+    
     args = parser.parse_args()
     return(args)
 
@@ -56,15 +64,27 @@ if __name__ == '__main__':
         # Create experiment directory
         experiment_dir = save_dir / input_arguments.weight_tissue
         experiment_dir.mkdir(parents=True,  exist_ok=True)
-        output_filename = utils.construct_filename(experiment_dir, "output", ".pkl", "pipline", "version1")
+    
+    logger.info("Creating pipline {}... ".format(input_arguments.weight_tissue))
+    
+    if input_arguments.normalized:
+        output_filename = utils.construct_filename(experiment_dir, "output", ".pkl", "feature_eng", input_arguments.weight_tissue)
         # Check output file is exist or not
         if utils.check_exist_files(output_filename):
             raise Exception("Results file " + str(output_filename) + " exist already. Please double-check.")
-    
-    logger.info("Creating pipline {}... ".format(input_arguments.weight_tissue))
-
-    pipeline = make_pipeline(
-         pp.CategoricalImputer_Education(variables=['FatherEducation', 'MotherEducation']),
-         pp.CategoricalEncoder_Income(variables=['Income'])
-    )
-    dm.RF_OOB_Dataset.save(pipeline, output_filename)
+        pipeline = make_pipeline(
+            pp.NormalizeDataTransformer()
+        )
+        joblib.dump(pipeline, output_filename)
+    else:
+        output_filename = utils.construct_filename(experiment_dir, "output", ".pkl", "feature_eng", "envir")
+        # Check output file is exist or not
+        if utils.check_exist_files(output_filename):
+            raise Exception("Results file " + str(output_filename) + " exist already. Please double-check.")
+        
+        pipeline = make_pipeline(
+            pp.CategoricalImputer_Education(variables=['FatherEducation', 'MotherEducation']),
+            pp.CategoricalEncoder_Income(variables=['Income'])
+        )
+        joblib.dump(pipeline, output_filename)
+        
