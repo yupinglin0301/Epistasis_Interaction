29c6258959893991139ff03c9794ad6e7bef4bbc
yupinglin0301 <yupinlin50@gmail.com>
2024-03-25 10:15:55 +0800

    feats: add etl function

--- 01_Preprocessing/etl_for_predictor.py
+++ 01_Preprocessing/etl_for_predictor.py
@@ -3,7 +3,52 @@ from pathlib import Path
 import preprocessors as pp
 import argparse
 import utils
-import joblib
+import dataset_model as dm
+import yaml
+import sys
+
+
+
+# Extraction
+def extract(data_set):
+    """
+    Read Excel file
+    """
+    df = data_set.all_gwas_df
+    
+    return df
+
+def transform_data(df):
+    """
+    Perform data transformation using a pipeline of preprocessing steps.
+
+    :param df: DataFrame containing the data to be transformed.
+    :return Transformed DataFrame.
+    """
+    pipeline = make_pipeline(
+            pp.NormalizeDataTransformer(column_names=df.columns.tolist()[5:]),
+            pp.CategorialEncoder_Education(variables=['FatherEducation', 'MotherEducation']),
+            pp.CategorialEncoder_Income(variables=['Income'])   
+    )
+    
+    transformed_data = pipeline.fit_transform(df)
+    return transformed_data
+   
+
+# Loading
+def load_data(dataset, output_filename):
+    """
+    Extracts data from the dataset, performs transformation, and saves the result as a csv file.
+    
+    :param dataset: The dataset object containing the data to be loaded.
+    :param output_filename: The name of the output file to save the transformed data.
+    """
+    extracted_data = extract(dataset)
+    transformed_data = transform_data(extracted_data)
+    
+    # Save transformed data as a csv file
+    transformed_data.to_csv(output_filename, sep="\t", index=False)
+   
 
 
 """
@@ -11,9 +56,8 @@ Functions for making pipline for feature engineering
 
 Usage:
     
-    nohup python run_making_pipline.py \
-      --weight_tissue "Brain_Amygdala" \
-      --normalized  >  /exeh_4/yuping/Epistasis_Interaction/01_Preprocessing/Log/nohup.txt &
+    nohup python etl_for_predictor.py \
+      --weight_tissue "Brain_Amygdala" > /exeh_4/yuping/Epistasis_Interaction/01_Preprocessing/Log/nohup.txt &
       
 Output:
 Feature engineering pipeline for specific imputed brain tissue and environmental factors
@@ -36,13 +80,7 @@ def process_args():
         action="store",
         help="Data directory where the phenotype and genotype matrix are stored."
     )
-    
-    parser.add_argument(
-        '--normalized',
-        action="store_true",
-        help="Normalized the gene expression dataset or not."
-    )
-    
+
     args = parser.parse_args()
     return(args)
 
@@ -51,49 +89,42 @@ if __name__ == '__main__':
     # process command line arguments
     input_arguments = process_args()
     # set up logging
-    if input_arguments.normalized:
-        logger = utils.logging_config(input_arguments.weight_tissue + "Normalized")
-    else:
-        logger = utils.logging_config(input_arguments.weight_tissue + "Feature_Engineering")
+    logger = utils.logging_config(input_arguments.weight_tissue + "Predictor")
+
     # set up repo_directory
     repo_root = Path(__file__).resolve().parent.parent
+    # set up working directory
+    work_dir = repo_root / str("01_Preprocessing")
+    # set up result directory
+    save_dir = work_dir.joinpath("results")
+    
+    #loading configure file
+    configure_file = work_dir.joinpath("config.yaml")
+    try:
+        with open(configure_file) as infile:
+            load_configure = yaml.safe_load(infile)
+    except Exception:
+            sys.stderr.write("Please specify valid yaml file.")
+            sys.exit(1)
     
  
-    
-    logger.info("Check if all files and directories exist ... ")
+    logger.info("Check if all files and directories exist ...")
     # Check save directory if is exist or not
-    work_dir = repo_root / str("01_Preprocessing")
-    save_dir = work_dir.joinpath("results")
     if not utils.check_exist_directories(save_dir):
         raise Exception("The directory" + str(save_dir) + "not exist. Please double-check.")
     else:
         # Create experiment directory
         experiment_dir = save_dir / input_arguments.weight_tissue
         experiment_dir.mkdir(parents=True,  exist_ok=True)
-    
-    logger.info("Creating pipline {}... ".format(input_arguments.weight_tissue))
-    
-    if input_arguments.normalized:
+        output_filename = utils.construct_filename(experiment_dir, "feature", ".csv", "predictor")
         
-        logger.info("Normalized gene expression data")
-        output_filename = utils.construct_filename(experiment_dir, "output", ".pkl", "normalized", input_arguments.weight_tissue)
         # Check output file is exist or not
         if utils.check_exist_files(output_filename):
-            raise Exception("Results file " + str(output_filename) + " exist already. Please double-check.")
+            raise Exception("Results files exist already. Please double-check.")
         
-        pipeline = make_pipeline(
-            pp.NormalizeDataTransformer()
-        )
-        joblib.dump(pipeline, output_filename)
-    else:
-        output_filename = utils.construct_filename(experiment_dir, "output", ".pkl", "feature_eng", "envir")
-        # Check output file is exist or not
-        if utils.check_exist_files(output_filename):
-            raise Exception("Results file " + str(output_filename) + " exist already. Please double-check.")
-        
-        pipeline = make_pipeline(
-            pp.CategoricalImputer_Education(variables=['FatherEducation', 'MotherEducation']),
-            pp.CategoricalEncoder_Income(variables=['Income'])
-        )
-        joblib.dump(pipeline, output_filename)
-        
+    logger.info("Conduct ETL pipeline ...")
+    GTEX_Dataset = dm.GTEX_raw_Dataset.from_config(config_file=load_configure, 
+                                                   weight_tissue=input_arguments.weight_tissue)
+    
+    load_data(dataset=GTEX_Dataset, output_filename=output_filename)
+    logger.info("Save predictor feature to {}".format(output_filename))  
